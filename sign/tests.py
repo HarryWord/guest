f37ab2e78d479b4473994dce00f6d519a53007d8
HarryWord <1453393593@qq.com>
2020-05-27 23:01:49 +0800

    add a test

--- sign/tests.py
+++ sign/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from sign.models import Event,Guest
 
 # Create your tests here.
+class ModelTest(TestCase):
+    def setUp(self) :
+        Event.objects.create(id=1,name="oneplus 3 event",status = True,limit = 2000, address = '798 in Beijing', start_time = '2020-05-30 10:00:00')
+        Guest.objects.create(id = 1, event_id = 1, realname = 'Alen', phone = '17671468132', email = 'alen@mail.com', sign = False)
+
+        def test_event_models(self):
+            result = Event.objects.get(name="oneplus 3 event")
+            self.assertEqual(result.address, "798 in Beijing")
+            self.assertTrue(result.status)
+
+        def test_guest_models(self):
+            result = Guest.objects.get(phone = '17671468132')
+            self.assertEqual(result.realname,"Alen")
+            self.assertFalse(result.sign)
